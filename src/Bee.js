51730764bf09982b9c82672bfaba72350d2b10b0
Matt Violet <mattviolet@gmail.com>
2019-04-15 12:28:58 -0700

    All tests pass

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,22 @@
 var Bee = function() {
-};
+	// call the Grub superclass
+	Grub.call(this);
+	// an age property that is set to 5
+	this.age = 5;
+	// a color property that is set to yellow
+	this.color = 'yellow';
+	// a job property that is set to keep on growing
+	this.job = 'keep on growing';
+};	
+	
+// set the prototype
+Bee.prototype = Object.create(Grub.prototype);
+// set the constructor
+Bee.prototype.constructor = Bee;
+	
+
+
+
+
+
 
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,21 @@
 var HoneyMakerBee = function() {
+	Bee.call(this);
+	this.age = 10;
+	this.job = 'make honey';	
+	this.honeyPot = 0;	
+	// this.makeHoney = function(){
+	// 	this.honeyPot++
+	// }
+	// this.giveHoney = function(){
+	// 	this.honeyPot--
+	// }
 };
 
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+HoneyMakerBee.prototype.makeHoney = function(){
+	this.honeyPot++
+};
+HoneyMakerBee.prototype.giveHoney = function(){
+	this.honeyPot--
+};
